256339b43795125e48c7eb71ae878253651c276a
Brendan McDonald <hello.brendanm@gmail.com>
2024-12-17 13:39:23 -0500

    update neotree and wezterm configs

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,6 +3,4 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-p>", "<leader>ff")
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -14,16 +14,44 @@ config.hide_tab_bar_if_only_one_tab = false
 config.window_decorations = "RESIZE"
 config.font = wezterm.font("Dank Mono")
 config.font_size = 16.0
+config.max_fps = 165
 -- Spawn a zsh shell in login mode
 -- config.default_prog = { "/bin/zsh", "-l" }
 -- config.default_cwd = "/Users/brendanmcdonald/Documents/mmhmm-tv/"
 --config.window_background_opacity = 0.9
 config.macos_window_background_blur = 10
 -- mux
-config.leader = { key = "q", mods = "ALT", timeout_milliseconds = 2000 }
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
 config.keys = {
 	{ key = "l", mods = "ALT", action = wezterm.action.ShowLauncher },
 	{ key = "e", mods = "CTRL", action = wezterm.action.EmitEvent("start-work") },
+	{ key = "-", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+	{ key = "\\", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+	{ key = "s", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+	{ key = "v", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+	{ key = "o", mods = "LEADER", action = "TogglePaneZoomState" },
+	{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+	{ key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+	{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+	{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+	{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+	{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+	{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
+	{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
+	{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+	{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
+	{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
+	{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
+	{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
+	{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
+	{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
+	{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
+	{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
+	{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
+	{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
+	{ key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+	{ key = "d", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+	{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
 }
 -- config.keys = {
 -- 	{
@@ -69,14 +97,21 @@ wezterm.on("start-work", function(cmd)
 		cwd = project_dir,
 		args = args,
 	})
-	local gitTab, gitPane = window:spawn_tab({ cwd = project_dir })
+	local gitPane = pane:split({ size = 0.3 })
 	gitPane:send_text("lazygit\n")
-	local nodeTab, nodePane = window:spawn_tab({ cwd = project_dir })
+	local nodePane = gitPane:split({ args = { "bottom" } })
 	nodePane:send_text("npm run dev:tv\n")
-	--
+	pane:activate()
 	pane:send_text("nvim ./\n")
-	tab:activate()
-	-- local editor_pane = pane:s\n")
+	tab:set_zoomed()
+	-- local gitTab, gitPane = window:spawn_tab({ cwd = project_dir })
+	-- gitPane:send_text("lazygit\n")
+	-- local nodeTab, nodePane = window:spawn_tab({ cwd = project_dir })
+	-- nodePane:send_text("npm run dev:tv\n")
+	-- --
+	-- pane:send_text("nvim ./\n")
+	-- tab:activate()
+	-- -- local editor_pane = pane:s\n")
 	mux.set_active_workspace("work")
 end)
 
