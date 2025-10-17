8a8e803b7c038d673b88f4719f3071eee45b0930
Brendan McDonald <hello.brendanm@gmail.com>
2025-10-17 12:07:35 -0400

    override for kanagawa

--- neovim/.config/nvim/lua/plugins/colors.lua
+++ neovim/.config/nvim/lua/plugins/colors.lua
@@ -13,5 +13,16 @@ return {
     'rebelot/kanagawa.nvim',
     lazy = false,
     priority = 1000,
+    opts = {
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = 'none',
+            },
+          },
+        },
+      },
+    },
   },
 }
